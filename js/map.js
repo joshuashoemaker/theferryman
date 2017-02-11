0d42b76dbc7bb412fe527bcf1781affeec4eb35d
Joshua Shoemaker <joshuashoemaker@takethisstudios.com>
2017-02-11 11:28:17 -0600

    refactor: seperated out google map script

---
+++ js/map.js
@@ -0,0 +1,33 @@
+            let map = {};
+            let markers = [];
+            function initMap() {
+
+                var initPost = {lat: locationList[0].coord[0], lng: locationList[0].coord[1]};
+                map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 15,
+                    center: initPost,
+                    styles: mapStyles,
+                    disableDefaultUI: true,
+                    
+                    selectMapLocation: function(locationName){
+                        markers.forEach(function(marker) {
+                            if(marker.title === locationName){
+                                marker.setAnimation(google.maps.Animation.BOUNCE);
+                            }
+                            if(marker.title != locationName){
+                                marker.setAnimation(google.maps.Animation.NONE);
+                            }
+                        }, this);
+                    }
+            });
+            locationList.forEach(function(loc) {
+                let marker = new google.maps.Marker({
+                    position: {lat: loc.coord[0], lng: loc.coord[1]},
+                    animation: google.maps.Animation.DROP,
+                    title: loc.name,
+                    map: map
+                });
+            markers.push(marker);
+        }, this);
+        
+          }
