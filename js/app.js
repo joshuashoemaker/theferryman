ae49816e4943e35235d7ee665e9b44ced8a8eb34
Joshua Shoemaker <joshuashoemaker@takethisstudios.com>
2017-02-15 16:42:29 -0600

    fix: getFlickrPhotos() now returns proper array

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ let ViewModel = function(){
     var self = this;
 
     this.visible = ko.observable(false);
-    this.locations = ko.observableArray(makeLocsObserve(locationList));
+    this.locations = ko.observableArray(createObservableLocs(locationList));
 
     this.selectedLocation = ko.observable(new Location({
         name: "Select Location to Info",
@@ -30,32 +30,41 @@ let ViewModel = function(){
         console.log("toggle");
         self.selectedInfoVisible(!self.selectedInfoVisible());
     }
-
 }
 
+
 let Location = function(data){
+    let self = this;
 
     this.name = ko.observable(data.name);
     this.description = ko.observable(data.description);
     this.coord = ko.observable(data.coord);
+    this.photos = ko.observableArray();
 
+    (function(){
+        let query = config.flickrQuery(data.flickrKey);
+        let promise = flickrPhotosPromise(query);
+        
+        promise.done(function(response){
+            let photos = getFlickrPhotos(response);
+            self.photos(photos);
+        });
+    })();
 }
+
 
 //takes in the array of locations and stores the observable
 //data in a new object and pushes that object into a new array
 //then returns that array
-function makeLocsObserve(list){
+function createObservableLocs(list){
     let newList = [];
     list.forEach(function(loc) {
-        newLoc = {
-            name: ko.observable(loc.name),
-            description: ko.observable(loc.description),
-            coord: ko.observableArray(loc.coord)
-        }
+        let newLoc = new Location(loc);
         newList.push(newLoc);
     }, this);
     return newList;
 }
+
 
 //This function takes in a url endpoint to the flickr api
 //the query url is generated in the config file. On the config
@@ -74,12 +83,13 @@ function flickrPhotosPromise(flickrQuery){
 //then returns the array created
 function getFlickrPhotos(data){
         let photos = data.photos.photo;
+        let newPhotos = []
         photos.forEach(function(p){
             if(p.url_o != undefined){
-                photos.push(p.url_o);
+                newPhotos.push(p.url_o);
             }
         });
-        return photos;
+        return newPhotos;
 }
 
 
