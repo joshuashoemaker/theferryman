98d80b73cdf43322a7bb66ca56891cb1543ab664
Joshua Shoemaker <joshuashoemaker@takethisstudios.com>
2017-02-15 16:37:48 -0600

    fix: lowered the image count from Flickr

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const config = {
     flickrQuery: function(keyword){
         let flickrKey = "3dedccec23d1400ca6888444a10b5fea";
         let flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="
-                        + flickrKey +"&format=json&nojsoncallback=1&tags=" + keyword + "&extras=url_o&per_page=10";
+                        + flickrKey +"&format=json&nojsoncallback=1&text=" + keyword + "&extras=url_o&per_page=5";
         return flickrUrl;
     }
 }
