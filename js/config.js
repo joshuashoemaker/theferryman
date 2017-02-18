90261a5f662beb6203fcf63efb828fd385a66628
Joshua Shoemaker <joshuashoemaker@takethisstudios.com>
2017-02-18 09:58:09 -0600

    fix: increased amount of images called from flickr

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const config = {
     flickrQuery: function(keyword){
         let flickrKey = "3dedccec23d1400ca6888444a10b5fea";
         let flickrUrl = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="
-                        + flickrKey +"&format=json&nojsoncallback=1&text=" + keyword + "&extras=url_o&per_page=5";
+                        + flickrKey +"&format=json&nojsoncallback=1&text=" + keyword + "&extras=url_o&per_page=25";
         return flickrUrl;
     }
 }
