7cc4273f71b071a0dc60f5ec0db11ca1fe57f005
Joshua Shoemaker <joshuashoemaker@takethisstudios.com>
2017-02-18 22:51:11 -0600

    refact: removed descriptions prop

--- js/locations.js
+++ js/locations.js
@@ -3,12 +3,6 @@ let locationList = [
         name: "The church of Santa Margherita de' Cerchi",
         flickrKey: "Santa%20Margherita%20de'%20Cerchi",
         wikiKey: "Santa%20Margherita%20de'%20Cerchi",
-        description: "From the Piazza Duomo, Via dello Studio will take you to Via del Corso where "
-        +               "you can pass underneath the archway of Via Margherita to the Church of Santa "
-        +               "Margherita de' Cerchi. This is the very church where the nine-year-old Dante "
-        +               "first saw and fell in love with Beatrice. He was so taken by her from this one "
-        +               "meeting that she became his life long muse and true love. He would follow her "
-        +               "around the city just to get glimpses of her, even though he barely ever spoke to her.",
         coord: [43.7712899, 11.2219466],
         address: "",
         keywords: ["church"]
@@ -17,12 +11,6 @@ let locationList = [
         name: "The Bargello",
         flickrKey: "The%20Bargello",
         wikiKey: "Bargello",
-        description: "During Dante's time it was "
-        +               "the headquarters of the City Council of Florence. It was here that Dante's "
-        +               "exile from the city was proclaimed. Inside the chapel of the Bargello is a "
-        +               "fresco attributed to none other than Giotto, who was a contemporary of Dante. "
-        +               "One of the most well known and quite possibly the oldest portrait of Dante depicts "
-        +               "him in Paradise, one of the earliest known images of the poet.",
         coord: [43.7703548, 11.2573951],
         address: "",
         keywords: []
@@ -31,13 +19,6 @@ let locationList = [
         name: "Baptistery of San Giovanni",
         flickrKey: "Baptistery%20of%20San%20Giovanni",
         wikiKey: "Florence%20Baptistery",
-        description: "During Dante's time it was where Dante was christened and where he hoped in vain to "
-        +               "receive the poet's crown he knew to be his due. Inside the Cathedral there is a "
-        +               "painting by Domenico di Michelino showing Dante illuminating Florence with his Poem "
-        +               "(1465), and outside it there is the 'Sasso di Dante', from which the poet is supposed "
-        +               "to have observed the construction of the Cathedral (begun in 1296). From the Via "
-        +               "Proconcolo we reach the Corso, site the old Porta San Piero, and pass the houses of the "
-        +               "Portinari, where Dante's beloved Beatrice lived.",
         coord: [43.7731154, 11.2528292],
         address: "Piazza del Duomo, 50123 Firenze, Italy",
         keywords: ["church"]
@@ -46,10 +27,6 @@ let locationList = [
         name: "Santa Croce",
         flickrKey: "Santa%20Croce",
         wikiKey: "Basilica%20of%20Santa%20Croce,%20Florence",
-        description: "The old tower houses of the Alberti, along Via de' Neri and Via della Ninna to the Palazzo "
-        +               "dei Priori (later known as Palazzo Vecchio, 1299), which stands beside the Romanesque "
-        +               "church of San Pier Scheraggio (later incorporated into the Uffizi), where Dante took part "
-        +               "in city assemblies.",
         coord: [43.7682442,11.2593632],
         address: "",
         keywords: []
@@ -58,10 +35,6 @@ let locationList = [
         name: "Duomo",
         flickrKey: "Duomo%20of%20Florence",
         wikiKey: "Duomo%20of%20Florence",
-        description: "Florence's cathedral stands tall over the city with its magnificent Renaissance dome "
-        +               "designed by Filippo Brunelleschi, with the baptistery right across. The cathedral named "
-        +               "in honor of Santa Maria del Fiore is a vast Gothic structure built on the site of the 7th "
-        +               "century church of Santa Reparata, the remains of which can be seen in the crypt.",
         coord: [43.773145,11.2537662],
         address: "50123 Florence, Italy",
         keywords: ["church"]
